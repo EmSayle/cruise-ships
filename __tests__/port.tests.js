f899b1d41b774f57119e911b337be8a3784868fb
Emily Sayle <emsayle@gmail.com>
2018-09-19 18:15:59 +0100

    added tests and code for set sail method and for docking at a new port

---
+++ __tests__/port.tests.js
@@ -0,0 +1,14 @@
+const Port = require('../src/port')
+
+describe('Port', () => {
+  it('returns a new port object', () => {
+    expect(new Port()).toBeInstanceOf(Object);
+  });
+});
+
+describe('Port', () => {
+  it('checks that the port has a name', () => {
+    const port = new Port('Saville');
+    expect(port.name).toEqual('Saville');
+  });
+});
--- __tests__/ship.tests.js
+++ __tests__/ship.tests.js
@@ -10,11 +10,11 @@ describe('Ship', () => {
   it('sets the ships starting port', () => {
     const ship = new Ship('Lisbon');
     expect(ship.startingPort).toEqual('Lisbon');
-  })
-})
+  });
+});
 
 describe('ship set sail method', () => {
   const ship = new Ship('Lisbon');
   ship.setSail();
   expect(ship.startingPort).toBeFalsy();
-})
+});
